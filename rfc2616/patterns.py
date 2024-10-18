b6b60e080feb3e21709770b74ddd340f40b0617d
Robert Royals <robert.royals@protonmail.com>
2024-10-18 15:45:05 +0100

    Add more alpha cases

--- rfc2616/patterns.py
+++ rfc2616/patterns.py
@@ -1,3 +1,18 @@
+def octet(val: bytes) -> bool:
+    # OCTET          = <any 8-bit sequence of data>
+    return len(val) == 1
+
+
 def upalpha(val: bytes) -> bool:
     # UPALPHA        = <any US-ASCII uppercase letter "A".."Z">
     return len(val) == 1 and (b"A" <= val <= b"Z")
+
+
+def loalpha(val: bytes) -> bool:
+    # LOALPHA        = <any US-ASCII lowercase letter "a".."z">
+    return len(val) == 1 and (b"a" <= val <= b"z")
+
+
+def alpha(val: bytes) -> bool:
+    # ALPHA          = UPALPHA | LOALPHA
+    return upalpha(val) or loalpha(val)
--- rfc2616/tests/test_patterns.py
+++ rfc2616/tests/test_patterns.py
@@ -1,6 +1,11 @@
 from unittest import TestCase
 
-from rfc2616.patterns import upalpha
+from rfc2616.patterns import (
+    octet,
+    loalpha,
+    upalpha,
+    alpha,
+)
 
 
 class TestUPALPHA(TestCase):
@@ -12,3 +17,34 @@ class TestUPALPHA(TestCase):
         self.assertFalse(upalpha(b"."))
         self.assertFalse(upalpha(b"AA"))
         self.assertFalse(upalpha(b""))
+
+
+class TestLOALPHA(TestCase):
+    def test_loalpha(self) -> None:
+        self.assertTrue(loalpha(b"b"))
+        self.assertTrue(loalpha(b"a"))
+        self.assertTrue(loalpha(b"z"))
+        self.assertFalse(loalpha(b"A"))
+        self.assertFalse(loalpha(b"."))
+        self.assertFalse(loalpha(b"aa"))
+        self.assertFalse(loalpha(b""))
+
+
+class TestALPHA(TestCase):
+    def test_alpha(self) -> None:
+        self.assertTrue(alpha(b"a"))
+        self.assertTrue(alpha(b"A"))
+        self.assertTrue(alpha(b"z"))
+        self.assertTrue(alpha(b"Z"))
+        self.assertFalse(alpha(b"1"))
+        self.assertFalse(alpha(b"\0"))
+        self.assertFalse(alpha(b""))
+        self.assertFalse(alpha(b"AA"))
+
+
+class TestOctet(TestCase):
+    def test_octet(self) -> None:
+        for i in range(256):
+            self.assertTrue(octet(bytes([i])))
+        self.assertFalse(octet(b""))
+        self.assertFalse(octet(b"AA"))
